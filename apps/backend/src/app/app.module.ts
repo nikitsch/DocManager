d5581940da364524387bae7dc3b5870f08bb077a
nikitsch <nikitsch5@gmail.com>
2025-01-08 13:50:37 +0300

    feat: db

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      autoLoadEntities: true,
+      synchronize: true, //TODO swith off in production
+      dropSchema: false,
+      logging: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
