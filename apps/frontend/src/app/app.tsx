6d8672c8d85ae16a00c56d871b7c01e82ffd7974
nikitsch <nikitsch5@gmail.com>
2025-02-01 16:16:24 +0300

    init: front

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,11 +1,11 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend" />
+      Hello
     </div>
   );
 }
